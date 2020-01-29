5c396537ca7b15e789cb8b268b675f894e8e53dd
testingship19 <59651221+testingship19@users.noreply.github.com>
2020-01-29 16:55:51 +0530

    Update Flip_scrap.py

--- medium/Flip_scrap.py
+++ medium/Flip_scrap.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[16]:
-
-
+#importing necessary libraries
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
 
+# extracting urls of all the pages
 
-# In[3]:
-
-
-url = "https://www.flipkart.com/mobile-accessories/cases-and-covers/pr?sid=tyy%2C4mr%2Cq2u&otracker=nmenu_sub_Electronics_0_Mobile+Cases&page=1"
-results = requests.get(url) #request to fetch the url    
-soup = BeautifulSoup(results.text, 'html.parser') #parsing the url
-pages_url =[]
+pages_url =[] # array for storing url of all the pages
 new_url = "https://www.flipkart.com/mobile-accessories/cases-and-covers/pr?sid=tyy%2C4mr%2Cq2u&otracker=nmenu_sub_Electronics_0_Mobile+Cases&page=1"
+
+# the url of the next/previous page should be compared with the url of the current page.
+# This method works for this website as only the last numeral changes from current to next/previous website.
+# Method may vary for different types of websites.
+
+
 j=1
-i=2
-while j<=50:
+i=2                   # since the url ending with '1' is already stored in new_url and gets appended in pages_url.
+while j<=50:          #the 51st page of this website returns an error. This means that we have 50 pages to extract urls from.
     j=j+1
     pages_url.append(new_url)
     new_url = "https://www.flipkart.com/mobile-accessories/cases-and-covers/pr?sid=tyy%2C4mr%2Cq2u&otracker=nmenu_sub_Electronics_0_Mobile+Cases&page="+ str(i)
     i=i+1
 
+pages_url  #displaying all the url of all the pages.
+products =[] # for storing product names
+prices= [] #for storing corresponding product prices.
 
-# In[4]:
-
-
-products =[]
-prices= []
 i=0
 for page in pages_url:
-    results2 = requests.get(page)
-    soup2 = BeautifulSoup(results2.text, 'html.parser')
+    results = requests.get(page)
+    soup = BeautifulSoup(results.text, 'html.parser')
     for x in soup2.findAll('div', attrs={'class':'_3liAhj'}):
         names = x.find('a', attrs={'class':'_2cLu-l'})
         cost =  x.find('div', attrs={'class':'_1vC4OE'})
@@ -44,29 +41,5 @@ for page in pages_url:
         print (products[i] + ":" + prices[i])
         i = i+1
 
-
-# In[15]:
-
-
 final_data = pd.DataFrame({'Name': products, 'Price': prices})
-[x[1:] for x in final_data.Price]
-final_data
-
-
-# In[14]:
-
-
-final_data.to_csv('test.csv')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+final_data.to_csv('test.csv') # 'test' name can changed to any name of choice
